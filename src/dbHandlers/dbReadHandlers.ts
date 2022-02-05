efb04dc1bbb05ac0c32e35c87fe26b04fda407ce
Jenaro Garcia <jjegarcia@hotmail.com>
2022-02-05 20:24:29 +0000

    tidyup

--- src/dbHandlers/dbReadHandlers.ts
+++ src/dbHandlers/dbReadHandlers.ts
@@ -1,19 +1,6 @@
 import {db} from "../firebaseConfig";
 import {writeSerial} from "../serialHandlers/serial";
 
-// export function testRead() {
-//     console.log("Reading");
-//     db.ref("Letterbox_database").child("event_list").get().then((snapshot) => {
-//         if (snapshot.exists()) {
-//             console.log(snapshot.val());
-//         } else {
-//             console.log("No data available");
-//         }
-//     }).catch((error) => {
-//         console.error(error);
-//     });
-// }
-
 export default function readFirebaseData(serialport: any) {
     const mailboxOutboxRef = db.ref("Letterbox_database").child("outbox");
     mailboxOutboxRef.on('value', (snapshot: any) => {
--- src/dbHandlers/dbWriteHandlers.ts
+++ src/dbHandlers/dbWriteHandlers.ts
@@ -1,11 +1,5 @@
 import {db} from "../firebaseConfig";
 
-// export function testWrite() {
-//     console.log("Writting");
-//     const obj = {name: 'testName6', id: 10};
-//     db.ref('testDir').set(obj, (error) => error ? console.log(error) : console.log('success o'));
-// }
-
 export default function writeFirebaseData(data: any) {
     const mailboxInboxRef = db.ref("Letterbox_database").child("inbox");
     mailboxInboxRef.set(data, (error) => error ? console.log(error) : console.log('f:==>' + data));
