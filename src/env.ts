fec05cb3b1f741b8c9a6cfc12b429121d8facd64
Jenaro Garcia <jjegarcia@hotmail.com>
2022-03-05 14:51:31 +0000

    change || port

--- src/env.ts
+++ src/env.ts
@@ -7,5 +7,5 @@ function getNumber(env: string | undefined, def: number): number {
     return env !== undefined ? Number(env) : def
 }
 
-export const PORT = getNumber(process.env.PORT, 5002)
+export const PORT = process.env.PORT || 5002 //getNumber(process.env.PORT, 5002)
 export const RESPONSE_DELAY = getNumber(process.env.RESPONSE_DELAY, 0)
