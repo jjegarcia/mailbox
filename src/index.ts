639b28ab7c96fbd30826df53117da6d968046596
Jenaro Garcia <jjegarcia@hotmail.com>
2022-03-05 12:37:48 +0000

    middleware failing

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,8 @@ import {closeSerial, getSerialPort, openSerial, readSerialListener,} from "./ser
 import readFirebaseData from "./dbHandlers/dbReadHandlers"
 // import {a2h,a22h,a24h} from "./hex/hex"
 // console.log(a24h('A1B0'))
-import {parseAccel,} from "./serialHandlers/accelParser"
-console.log(parseAccel('[FX0CAAAAAAAAAAAA]'))
+// import {parseAccel,} from "./serialHandlers/accelParser"
+// console.log(parseAccel('[FX0CAAAAAAAAAAAA]'))
 // import {parseButton,} from "./serialHandlers/buttonParser"
 // console.log(parseButton('[3P0201]'))
 // import {parseLed,} from "./serialHandlers/ledParser"
@@ -17,28 +17,26 @@ console.log(parseAccel('[FX0CAAAAAAAAAAAA]'))
 // console.log(parseTemperature('[0T04C001]'))
 
 
-// const app = express()
-// const initDb= false
-// const initSerial= false
-//
-// // SERIAL INTERFACE
-// // const serialport = getSerialPort('/dev/tty.usbmodem02691', 9600)
-// const serialport = getSerialPort('/dev/ttyS0', 9600)
-// const parser = openSerial(serialport)
-// readSerialListener(parser)
-// closeSerial(serialport)
-// readFirebaseData(serialport,initDb)
-//
-// // MIDDLEWARE
-// app.use(cors())
-// app.use(logger())
-// app.use(responseDelay(RESPONSE_DELAY))
-//
-// // ENDPOINTS
-// app.post('/user', json(), addUser) //(endpointUrl,handlers)
-// app.get('/user', getUser)
-//
-// //MAIN
-// app.listen({port: PORT}, () => console.log(`Server running on port ${PORT}`))
-//
-//
+const app = express()
+const initDb= false
+const initSerial= false
+
+// SERIAL INTERFACE
+// const serialport = getSerialPort('/dev/tty.usbmodem02691', 9600)
+const serialport = getSerialPort('/dev/ttyS0', 9600)
+const parser = openSerial(serialport)
+readSerialListener(parser)
+closeSerial(serialport)
+readFirebaseData(serialport,initDb)
+
+// MIDDLEWARE
+app.use(cors())
+app.use(logger())
+app.use(responseDelay(RESPONSE_DELAY))
+
+// ENDPOINTS
+app.post('/user', json(), addUser) //(endpointUrl,handlers)
+app.get('/user', getUser)
+
+//MAIN
+app.listen({port: PORT}, () => console.log(`Server running on port ${PORT}`))
