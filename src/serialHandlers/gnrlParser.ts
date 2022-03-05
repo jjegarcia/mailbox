7b2087a0a0ef1cefcf774692795d14d89adff9c8
Jenaro Garcia <jjegarcia@hotmail.com>
2022-03-05 12:05:47 +0000

    fixed temperature

--- src/serialHandlers/gnrlParser.ts
+++ src/serialHandlers/gnrlParser.ts
@@ -9,13 +9,13 @@ return data.split(header)[1]
 }
 
 export function get4Number(data: string,start: any,end: any){
-return a24h(data.substring(start,end))
+return a24h(data.substring(start+2,end+2))
 }
 
 export function get2Number(data: string,start: any,end: any){
-return a22h(data.substring(start,end))
+return a22h(data.substring(start+2,end+2))
 }
 
 export function get1Number(data: string,start: any,end: any){
-return a2h(data.substring(start,end))
+return a2h(data.substring(start+2,end+2))
 }
